eae2fa9b2764ec9087431318afcfd517d0b6d012
guess-hwu <shany198@gmail.com>
2017-09-16 16:05:55 -0400

    address cleanup
    strip addressArrays

--- excel-cleanup.py
+++ excel-cleanup.py
@@ -2,5 +2,14 @@ from openpyxl import load_workbook
 wb = load_workbook('data.xlsx')
 site_sheet = wb['Site']
 
-for cell in site_sheet['D']:
-    print(cell.value)
+for cell in site_sheet['D']: #hard-code find address
+    address = cell.value
+    addressArray = address.split()
+    print(address)
+    for index, word in enumerate(addressArray):
+    	word = word.strip(',')
+    	word = word.strip('.')
+    	addressArray[index] = word
+
+for i in addressArray[::-1]:
+	print(addressArray[i]) #1: zipcode 2: state 3: city

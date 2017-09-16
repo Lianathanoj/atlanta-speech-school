aa9ca7416e0f3b61c0f910499026cf92f574b3ff
Jonathan Lian <jlian@gatech.edu>
2017-09-16 15:47:00 -0400

    find address column without hard coding

--- excel-cleanup.py
+++ excel-cleanup.py
@@ -1,6 +1,19 @@
 from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
 wb = load_workbook('data.xlsx')
 site_sheet = wb['Site']
 
-for cell in site_sheet['D']:
-    print(cell.value)
+def find_address_column():
+    for row in site_sheet.iter_rows(min_row=0, max_row=1):
+        for index, cell in enumerate(row):
+            if cell.value == "Site Address":
+                return get_column_letter(index)
+
+def cell_operations():
+    address_column = find_address_column()
+    for cell in site_sheet[address_column]:
+        print(cell.value)
+
+if __name__ == '__main__':
+    cell_operations()

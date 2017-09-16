8915994da4b9d8aedadaa0093cfdc4fcd2b52325
Jonathan Lian <jlian@gatech.edu>
2017-09-16 17:36:59 -0400

    lat long column works

--- excel-cleanup.py
+++ excel-cleanup.py
@@ -1,8 +1,9 @@
+# import pandas as pd
 from geocoder import google
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook('data.xlsx')
+wb = load_workbook('data - Copy.xlsx')
 site_sheet = wb['Site']
 
 def find_address_column():
@@ -15,14 +16,24 @@ cell_errors = []
 
 def cell_operations():
     address_column = find_address_column()
-    for cell in site_sheet[address_column]:
+    new_column_letter = get_column_letter(site_sheet.max_column + 1)
+    for index, cell in enumerate(site_sheet[address_column]):
         # print(cell.value)
         try:
-            print(cell, google(cell.value).latlng)
+            # print(cell, google(cell.value).latlng)
+            # print(new_column_letter + str(index + 1))
+            latlng = google(cell.value).latlng
+            lat, long = latlng[0], latlng[1]
+            site_sheet[new_column_letter + str(index + 1)] = str(lat) + ', ' + str(long)
         except ValueError:
+            print('test')
             cell_errors.append((cell, cell.value))
+    site_sheet[new_column_letter + '1'] = "Latitude/Longitude"
+
     print("These addresses could not be converted to latitude-longitude coordinates.")
     print(cell_errors)
+
+    wb.save('test.xlsx')
 
 if __name__ == '__main__':
     cell_operations()

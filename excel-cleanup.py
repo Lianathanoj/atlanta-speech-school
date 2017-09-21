c2c969d4546661ef12139e67a03074a56fe81d8e
Jonathan Lian <jlian@gatech.edu>
2017-09-21 02:29:57 -0400

    file name/path corrections

--- excel-cleanup.py
+++ excel-cleanup.py
@@ -1,7 +1,6 @@
 # import pandas as pd
-import tkinter as tk
 from tkinter import Tk
-from os import getcwd
+from os import getcwd, path
 from geocoder import google
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
@@ -9,10 +8,11 @@ from tkinter.filedialog import askopenfilename
 
 root = Tk()
 root.withdraw()
-file_name = askopenfilename(initialdir=getcwd(), title="Select an Excel file.",
+file_path = askopenfilename(initialdir=getcwd(), title="Select an Excel file.",
                             filetypes=[("Microsoft Excel Worksheet", ".xlsx")])
-wb = load_workbook(file_name)
+wb = load_workbook(file_path)
 site_sheet = wb['Site']
+cell_errors = []
 
 def find_address_column():
     for row in site_sheet.iter_rows(min_row=0, max_row=1):
@@ -20,11 +20,10 @@ def find_address_column():
             if cell.value == "Site Address":
                 return get_column_letter(index)
 
-cell_errors = []
-
 def cell_operations():
     address_column = find_address_column()
-    new_column_letter = get_column_letter(site_sheet.max_column + 1)
+    latitude_column_letter = get_column_letter(site_sheet.max_column + 1)
+    longitude_column_letter = get_column_letter(site_sheet.max_column + 2)
     for index, cell in enumerate(site_sheet[address_column]):
         # print(cell.value)
         try:
@@ -32,16 +31,21 @@ def cell_operations():
             # print(new_column_letter + str(index + 1))
             latlng = google(cell.value).latlng
             lat, long = latlng[0], latlng[1]
-            site_sheet[new_column_letter + str(index + 1)] = str(lat) + ', ' + str(long)
+            site_sheet[latitude_column_letter + str(index + 1)] = lat
+            site_sheet[longitude_column_letter + str(index + 1)] = long
         except ValueError:
             print('Could not read address correctly.')
             cell_errors.append((cell, cell.value))
-    site_sheet[new_column_letter + '1'] = "Latitude/Longitude"
+    site_sheet[latitude_column_letter + '1'] = "Latitude"
+    site_sheet[longitude_column_letter + '1'] = "Longitude"
 
     print("These addresses could not be converted to latitude-longitude coordinates.")
     print(cell_errors)
 
-    wb.save(file_name + '_MODIFIED')
+    file_name = path.basename(file_path)
+    index = file_name.find(".xlsx")
+    wb.save(file_name[:index] + '_MODIFIED' + file_name[index:])
 
 if __name__ == '__main__':
     cell_operations()
+    # print()
